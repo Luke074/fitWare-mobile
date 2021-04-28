92e7c290ae46412940cee9144d2705d85470a617
Luke074 <mendeslucas277@gmail.com>
2021-04-28 16:16:56 -0300

    tela de login feita, tela home em desenvolvimento

--- app/src/main/java/br/senai/sp/fitware/fragments/HomeFragment.kt
+++ app/src/main/java/br/senai/sp/fitware/fragments/HomeFragment.kt
@@ -1,10 +1,9 @@
 package br.senai.sp.fitware.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import br.senai.sp.fitware.R
 
 class HomeFragment : Fragment() {
 
@@ -13,8 +12,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         setHasOptionsMenu(true)
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
 }
