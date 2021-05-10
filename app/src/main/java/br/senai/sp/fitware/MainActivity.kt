84c4b21354342fcb3ff31fbf530759c564d1c3ca
Luke074 <mendeslucas277@gmail.com>
2021-05-10 16:51:45 -0300

    criando funcao para trocar de tela no navigation_bottom

--- app/src/main/java/br/senai/sp/fitware/MainActivity.kt
+++ app/src/main/java/br/senai/sp/fitware/MainActivity.kt
@@ -2,11 +2,22 @@ package br.senai.sp.fitware.gui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import br.senai.sp.fitware.R
+import br.senai.sp.fitware.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+
+    private lateinit var homeFragment: HomeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
