aa44fde8b03336e72f8f278dfd5d7861524e0e9b
Ricardo Lima <ricardolima26082003@gmail.com>
2021-06-30 09:05:40 -0300

    interação com api na nuvem

--- app/src/main/java/br/senai/sp/fitware/api/UrlApi.kt
+++ app/src/main/java/br/senai/sp/fitware/api/UrlApi.kt
@@ -3,7 +3,9 @@ package br.senai.sp.fitware.api
 class UrlApi {
     companion object{
 //        const val BASE_URL = "http://10.0.2.2:3333/"
-        const val BASE_URL = "http://10.0.0.104:3333/"
+//        const val BASE_URL = "http://10.0.0.104:3333/"
+            const val BASE_URL = "https://fitware-app-backend.herokuapp.com/"
+
         const val CONTENT_TYPE = "application/json"
     }
 }
